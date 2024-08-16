03c7b28a27bfb60b5df263edc4775ed3582298d3
onokoro0822 <watau0822@gmail.com>
2024-08-16 19:54:58 +0900

    change ejs files

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const path = require("path");
+const app = express();
 
-
+// EJSをテンプレートエンジンとして設定
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+// viewsフォルダのパスを設定
+app.set('views', path.join(__dirname, 'views'));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
+app.use(express.static('public'));
+
+
+// ルートにアクセスしたときにindex.ejsをレンダリング
+app.get("/", (req, res) => {
+    res.render("index", { message: "こんにちは、EJS!" });
 });
+
+app.get("/content", (req, res) => {
+    res.render("content");
+});
+
+app.get("/access", (req, res) => {
+    res.render("access");
+});
+
+app.get("/aboutus", (req, res) => {
+    res.render("about_us");
+});
+
+// サーバーを3000番ポートで起動
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
+
